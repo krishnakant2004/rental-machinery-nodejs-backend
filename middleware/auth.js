6561fcbcf3c205dd3b680148bde53cf075153c01
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-12 23:10:07 +0530

    necessary changes which should be there in repo

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,7 +20,7 @@ const auth = async (req, res, next) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findOne({ _id: decoded._id });
-        console.log(user);
+        // console.log(user);
 
         if (!user) {
             return res.status(401).json({success:false,message: 'User not found'});
